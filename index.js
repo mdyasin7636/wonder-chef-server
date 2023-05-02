3ba39521a928e60824b157f6066ff677ba4beeb2
mdyasin111 <mdyasin7636@gmail.com>
2023-05-02 20:06:18 +0600

    json file created

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Chef API is running on port: ${port}`)
+})
