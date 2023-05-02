703cad89ff8397170b0783a3d539d928d67f9772
mdyasin111 <mdyasin7636@gmail.com>
2023-05-03 05:51:13 +0600

    data shown in server side

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/allData.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(chefs);
+})
 
 app.get('/', (req, res) => {
     res.send('Server is running');
